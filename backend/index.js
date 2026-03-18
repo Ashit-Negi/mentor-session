32b76aa1a04be899bc36c88190c729d2a0b83dde
Ashit-Negi <sashitnegi@gmail.com>
2026-03-19 00:15:18 +0530

    day 1 setup frontend backend mongodb connection

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("api running...");
+});
+
+app.listen(5000, () => console.log("server is running "));
