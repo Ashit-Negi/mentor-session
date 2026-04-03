ce7965366d4606925bd17e2a1d57652eef80632c
Ashit-Negi <sashitnegi@gmail.com>
2026-04-03 16:26:30 +0530

    fixing socket.js

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const socket = io(BACKEND_URL, {
+  transports: ["websocket"],
   autoConnect: true,
 });
