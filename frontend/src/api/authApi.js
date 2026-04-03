683afa9bf15cceb0bdd6917b13bf196d0efe351d
Ashit-Negi <sashitnegi@gmail.com>
2026-04-03 17:26:48 +0530

    register issue

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,4 +1,4 @@
 import axios from "./index";
 
 export const loginUser = (data) => axios.post("/auth/login", data);
-export const registerUser = (data) => axios.post("/auth/signup", data);
+export const registerUser = (data) => axios.post("/auth/register", data);
